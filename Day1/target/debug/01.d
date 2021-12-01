82f46f06441710384b1d9e87cb871cd4787fc3fd
Djamal <rust.91@icloud.com>
2021-12-01 19:10:03 +0100

    Correction

---
+++ Day1/target/debug/01.d
@@ -0,0 +1,1 @@
+/Users/chessman/adventofcode_2021/aof21/target/debug/01: /Users/chessman/adventofcode_2021/aof21/src/01.rs
---
+++ Day1/target/debug/deps/01-0ae358e61d319374.d
@@ -0,0 +1,5 @@
+/Users/chessman/adventofcode_2021/aof21/target/debug/deps/01-0ae358e61d319374: src/01.rs
+
+/Users/chessman/adventofcode_2021/aof21/target/debug/deps/01-0ae358e61d319374.d: src/01.rs
+
+src/01.rs:
